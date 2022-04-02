dc10110c638c3bb15e047f0b5a01dd87a012f3b1
“Shikha” <shikha.bansal@prepladder.com>
2022-04-02 19:17:28 +0530

    minor-change

--- src/Components/user-mail.js
+++ src/Components/user-mail.js
@@ -1,27 +1,45 @@
 import React, { useState } from "react";
 
-const UserMail = ({ message, deletedMailId, getNumberOfUnreadMessages , getFlaggedData }) => {
-    const { mId, unread, subject, content ,flagged=false } = message
-    const [showContent, setShowContent] = useState(false)
-    
-    return (
-        <>
-         <div className="flex">
-            <div onClick={() => {
-                setShowContent(!showContent)
-                getNumberOfUnreadMessages({ ...message, unread: false })
-            }} className="training">{subject}</div>
-            <div className="flex">
-            <div onClick={() => deletedMailId(mId) } className="deleteIcon ">
-                <img src="./delete.png"/>
-            </div>
-            <div className="flagIcon ml-1" onClick={()=> getFlaggedData(mId,!flagged)}>{flagged? <img src="./flagged.png"/> : <img src="./flag.png"/>}</div>
-            </div>
-            </div>
-            {!showContent && <div>{content.slice(0, 100)}</div>}
-            {showContent && <div>{content}</div>}
-        </>
-    )
-}
+const UserMail = ({
+  message,
+  deletedMailId,
+  getNumberOfUnreadMessages,
+  getFlaggedData,
+}) => {
+  const { mId, unread, subject, content, flagged = false } = message;
+  const [showContent, setShowContent] = useState(false);
+
+  return (
+    <>
+      <div className="flex cursor-pointer">
+        <div
+          onClick={() => {
+            setShowContent(!showContent);
+            getNumberOfUnreadMessages({ ...message, unread: false });
+          }}
+          className="training"
+        >
+          {subject}
+        </div>
+        <div className="flex">
+          <div onClick={() => deletedMailId(mId)} className="deleteIcon ">
+            <img src="./delete.png" />
+          </div>
+          <div
+            className="flagIcon ml-1"
+            onClick={() => getFlaggedData(mId, !flagged)}
+          >
+            {flagged ? <img src="./flagged.png" /> : <img src="./flag.png" />}
+          </div>
+        </div>
+      </div>
+      {!showContent ? (
+        <div className="line-clamp">{content.slice(0, 100)}</div>
+      ) : (
+        <div>{content}</div>
+      )}
+    </>
+  );
+};
 
 export default UserMail;
