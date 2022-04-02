a27be6c8ab730da6b890b5ab54a3edc524831b31
“Shikha” <shikha.bansal@prepladder.com>
2022-04-02 18:51:05 +0530

    Outlook-Project

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
 import * as ReactDOMClient from 'react-dom/client';
 import './index.css';
-import App from './App';
+
 import Main from "./Components/main"
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore } from 'redux';
 
-// ///ACTIONS 
-// const fetchInboxData = () => {
-//   return {
-//     type: "FETCH_INBOX_DATA"
-//   }
-// }
-
-// //reducers
-// const fetchInboxDataReducer
-
-// let store = createStore
 
 const container = document.getElementById('root')
 const root = ReactDOMClient.createRoot(container);
